f36cc069a0148e0e8c2b73bd38dfd6f5a59e5208
inDemniFi <indemnifi@gmail.com>
2023-09-23 18:18:04 -0500

    main

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  entry: './App.tsx', // Updated entry point for the web
+  entry: './src/screens/home.js', // Updated entry point for the web
   output: {
     filename: 'bundle.js', // Name of the output bundle
     path: path.resolve(__dirname, 'dist'), // Output directory
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], // File extensions to resolve
+    extensions: ['.ts', '.tsx', '.js'], // File extensions to resolve
   },
   module: {
     rules: [
@@ -18,18 +17,17 @@ module.exports = {
         use: 'babel-loader', // Use Babel for these files
         exclude: /node_modules/, // Exclude node_modules directory
       },
+      {
+        test: /\.ts$/, // Match TypeScript files
+        use: 'ts-loader', // Use ts-loader for TypeScript files
+        exclude: /node_modules/, // Exclude node_modules directory
+      },
       // Add other loaders for CSS, images, etc. as needed
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Path to your HTML template
+      template: './index.html', // Path to your HTML template file
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'), // Serve content from the 'dist' directory
-    },
-    port: 3000, // Port for the development server
-  },
 };
