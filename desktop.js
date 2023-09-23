23b1d6c09adfa55d691b34383d48cba588023484
inDemniFi <indemnifi@gmail.com>
2023-09-23 15:27:10 -0500

    main

--- desktop.js
+++ desktop.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './src/screens/home';
+import App from './App'; // Import your existing mobile app components
 import DesktopSpecificComponent from './DesktopSpecificComponent'; // Import components specific to the desktop version
 import { useMediaQuery } from 'react-responsive';
 
@@ -11,12 +11,10 @@ const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' });
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     {isDesktop ? (
-      <>
-        <App /> {/* Render your mobile app */}
-        <DesktopSpecificComponent /> {/* Render desktop-specific components */}
-      </>
+      <DesktopSpecificComponent /> {/* Render desktop-specific components */}
     ) : (
       <App /> {/* Render your mobile app for smaller screens */}
     )}
   </React.StrictMode>
 );
+
