0ce75404e971912ad1b26f3fb579a33c54405daf
inDemniFi <indemnifi@gmail.com>
2023-09-24 13:00:46 -0500

    main

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,10 +1,8 @@
-// Button.tsx
-import { TouchableOpacity, Text } from 'react-native';
+import {TouchableOpacity, Text} from 'react-native';
 import React from 'react';
-import { styles } from '../styles/styles';
+import {styles} from '../styles/styles';
 
-
-const Button: React.FC<ButtonProps> = ({ onPress, title, ...props }) => {
+const Button = ({onPress, title, ...props}) => {
   return (
     <TouchableOpacity onPress={onPress} style={[styles.btn, props.style]}>
       <Text style={styles.btnText}>
